64736ec2e4e3b37377e4188b13f34cfdad336bb8
Dihee <hwangjihee94@gmail.com>
2022-11-15 21:07:45 +0900

    BaekJoon 14645

---
+++ Into-the-Algorithm/Into-the-Algorithm/BaekJoon/14645.swift
@@ -0,0 +1,16 @@
+//
+//  14645.swift
+//  Into-the-Algorithm
+//
+//  Created by Jihee hwang on 2022/11/15.
+//
+
+import Foundation
+
+func q14645() {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    for i in 0..<input[1] {
+        let input = readLine()!
+    }
+    print("비와이")
+}
