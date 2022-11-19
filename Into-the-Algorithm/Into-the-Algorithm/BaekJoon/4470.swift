b78c211516e018e1ec8f6b7bb734d5942b5f17c2
Dihee <hwangjihee94@gmail.com>
2022-11-19 22:59:39 +0900

    BaekJoon 4470

---
+++ Into-the-Algorithm/Into-the-Algorithm/BaekJoon/4470.swift
@@ -0,0 +1,15 @@
+//
+//  4470.swift
+//  Into-the-Algorithm
+//
+//  Created by Jihee hwang on 2022/11/19.
+//
+
+import Foundation
+
+func q4470() {
+    let number = Int(readLine()!)!
+    for i in 1...number {
+        print("\(i). \(readLine()!)")
+    }
+}
